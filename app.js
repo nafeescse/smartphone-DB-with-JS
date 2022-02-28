fcc44631eebcc8e3eb25296cb2b7d3725be47dd8
Nafees <abdur17@student.sust.edu>
2022-03-01 05:41:54 +0600

    details function added

--- app.js
+++ app.js
@@ -1,35 +1,66 @@
 const searchPhone = () => {
     const searchField = document.getElementById('searchField');
     const searchText = searchField.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(dataset => loadSearch(dataset.data))
-    // console.log(searchText);
-    searchField.value = '';
-}
-searchPhone();
-const loadSearch = allPhones => {
-    const parent = document.getElementById('parent-container');
-    parent.textContent = '';
-    const phones = allPhones.slice(0, 20);
-    console.log(phones);
-    for (const phone of phones) {
-        console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('col');
 
-        div.innerHTML = `
+    if (searchField.value == '') {
+        window.alert('Please search something...')
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(dataset => loadSearch(dataset.data))
+
+        searchField.value = '';
+    }
+}
+// searchPhone();
+const loadSearch = allPhones => {
+    if (allPhones.length === 0) {
+        window.alert("No phone's Found !!")
+    } else {
+        const status = document.getElementById('status');
+        status.textContent = '';
+        const p = document.createElement('p');
+        p.classList.add('h4');
+
+        // console.log(allPhones);
+        const parent = document.getElementById('parent-container');
+        parent.textContent = '';
+        const phones = allPhones.slice(0, 20);
+        p.innerText = `Showing ${phones.length}  results out of ${allPhones.length}`;
+        status.appendChild(p);
+        // console.log(phones);
+        for (const phone of phones) {
+            console.log(phone);
+            const div = document.createElement('div');
+            div.classList.add('col');
+
+            div.innerHTML = `
         <div class="card rounded-lg">
                   <img src="${phone.image}" class="card-img-top w-50 mx-auto my-4">
                   <div class="card-body d-flex flex-column  align-items-center">
                   <h2> <span class="fw-bold">Brand:</span> ${phone.brand}</h2>
                   <h2> <span class="fw-bold">Model:</span> ${phone.phone_name}</h2>
-                  <button class="mt-3 px-5 py-.5 rounded-xl bg-pink-600 text-white">Details</button>
+                  <button onclick='detailsLoad("${phone.slug}")' class="mt-3 px-5 py-0.5 rounded-xl bg-pink-600 text-white">Details</button>
                   </div>
                 </div>
         `;
-        parent.appendChild(div);
-
+            parent.appendChild(div);
+        }
     }
+}
+
+const detailsLoad = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(dataset => displayDetails(dataset.data))
+    // console.log(id);
+}
+
+const displayDetails = details => {
+
+    console.log(details.releaseDate);
+
 }
